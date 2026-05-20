3696a063ed7bfd9393b6cecfe717ac31a448bc90
Syko <91411604+Sykozard@users.noreply.github.com>
2026-05-20 13:26:49 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,9 @@ script.src = 'https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js';
 script.defer = true;
 document.head.appendChild(script);
 
+const params = new URLSearchParams(window.location.search);
+const user = params.get('user');
+
 window.OneSignalDeferred.push(async function (OneSignal) {
   await OneSignal.init({
     appId: "e46f14ac-f050-4201-a38f-1a2a861f5881",
@@ -22,6 +25,10 @@ window.OneSignalDeferred.push(async function (OneSignal) {
       enable: false,
     },
   });
+
+  if (user) {
+    await OneSignal.login(user);
+  }
 });
 
 createRoot(document.getElementById('root')!).render(<App />);
