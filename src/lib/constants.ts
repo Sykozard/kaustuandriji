96d0c420f50e4de06b12061330629db67b0c2c18
Syko <91411604+Sykozard@users.noreply.github.com>
2026-08-25 09:53:18 +0530

    feat: add all missing constants (VAULT_PASSWORD)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,6 @@
 // Relationship start date: April 13, 2025
 export const RELATIONSHIP_START = new Date(2025, 3, 13) // Month is 0-indexed, so 3 = April
 
-// Upload password for scrapbook
+// Passwords for various features
 export const UPLOAD_PASSWORD = 'analovesali69'
+export const VAULT_PASSWORD = 'analovesali69'
