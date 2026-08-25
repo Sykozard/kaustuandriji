81315c2f649f1b3054eb96de12b01ca7b0cc487b
Syko <91411604+Sykozard@users.noreply.github.com>
2026-08-25 09:43:17 +0530

    feat: add UPLOAD_PASSWORD constant to fix build

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,5 @@
 // Relationship start date: April 13, 2025
 export const RELATIONSHIP_START = new Date(2025, 3, 13) // Month is 0-indexed, so 3 = April
+
+// Upload password for scrapbook
+export const UPLOAD_PASSWORD = 'analovesali69'
